88f43e663defa06c2415a368556675731c36b3b0
edus44 <eduhhg@gmail.com>
2018-07-25 16:11:12 +0200

    converted to vue-cli-service-global

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <h1>Eduardo Hidalgo</h1>
+    <div class="links">
+      <a href="https://github.com/edus44">
+        <i class="fab fa-linkedin-in"></i>
+      </a>
+      <a href="https://github.com/edus44">
+        <i class="fab fa-github"></i>
+        edus44
+      </a>
+    </div>
+    
+    <a @click="folded = !folded" >
+      See games
+      <i class="fas" :class="folded?'fa-sort-up':'fa-sort-down'" ></i>
+    </a>
+    <div v-if="folded">
+      <Project v-for="project in projects" v-bind="project" :key="project.name" />
+    </div>
+  </div>
+</template>
+
+<script>
+import projects from './projects'
+import Project from './Project'
+
+export default {
+  components: {
+    Project,
+  },
+  data: () => ({
+    folded: false,
+    projects,
+  }),
+}
+</script>
+
+<style>
+body,
+html {
+  margin: 0;
+  padding: 0;
+  font-size: 10px;
+  text-align: center;
+}
+body {
+  font-family: 'Nunito', sans-serif;
+  color: rgb(20, 16, 14);
+}
+h1 {
+  font-size: 2rem;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  margin-top: 3rem;
+}
+.links a {
+  color: rgb(20, 16, 14);
+  text-decoration: none;
+  display: inline-block;
+  margin: 0 1rem;
+  font-size: 1.3rem;
+  font-weight: bold;
+}
+.links a:hover {
+  color: rgb(28, 160, 149);
+}
+</style>
---
+++ src/Project.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <a :href="link">
+      <b>{{name}}</b>
+    </a>
+    
+    <a :href="src">
+      <i class="fas fa-code"></i>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['name','src','link']
+
+}
+</script>
+
+<style>
+
+</style>
